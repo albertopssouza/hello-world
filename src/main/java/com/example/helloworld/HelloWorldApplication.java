3be31a72de084e1db58dc6c269684c6ce456c99b
albertopssouza <alberto.pssouza@gmail.com>
2020-09-26 15:07:25 -0400

    Segundo commit

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -2,7 +2,11 @@ package com.example.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
 public class HelloWorldApplication {
 
@@ -10,4 +14,13 @@ public class HelloWorldApplication {
 		SpringApplication.run(HelloWorldApplication.class, args);
 	}
 
+        @RestController
+        class HelloWorld {
+            
+            @GetMapping("/")
+            public String printHelloWorld() {
+                return "Hello World";
+            }
+            
+        }
 }
